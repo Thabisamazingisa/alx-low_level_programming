1af3f2082bfdb48e1470bfb8b275c501ac856089
Thabisa <tab95mazingisa@gmail.com>
2023-07-06 03:03:01 -0700

    Added 0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** binary_to_uint - Converts a bnary number to an unsigned int
+/**
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: String  with the binary number
  *
  * Return: the converted number
@@ -10,9 +11,9 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int num = 0;
 
-	if(!b)
-		return(0);
-	for (i =; b[i]; i++)
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,11 +6,11 @@
  * @index: index of the bit to set to 1
  * Return: 1 for success, -1 for failure
  */
-int set_bit(unsigned long unt *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
 	*n = (1UL << index) | *n;
-	return (1)
+	return (1);
 }
