edc100c1498745bee912d60794b27ebf781ca710
Thabisa <tab95mazingisa@gmail.com>
2023-07-10 00:53:46 -0700

    0x15-file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: Pointer to the name of the file to create.
+ * @text_content: Pointer to a string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, results = 1, count = 0;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREATE | O_WONLY | O_TRUNC, 0600);
+	if (ile == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[count])
+		{
+			count++;
+			results = write(file, text_content, count);
+		}
+		if (results == -1)
+			return (-1);
+		close(file);
+		return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: pointer to the file name.
+ * @text_content: String to edd to the end of the file
+ * Return: -1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w;
+
+	if (!filename)
+		return (-1);
+
+	o = open(filename, O_WONLY | O_APPEND);
+	if (o == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(o);
+		return (1);
+	}
+
+	w = write(o, text_content, strlen(text_content));
+	if (w == -1 || w != strlen(text_content))
+	{
+		close(o);
+		return (-1);
+	}
+	
+	close(o);
+	return (1);
+}
