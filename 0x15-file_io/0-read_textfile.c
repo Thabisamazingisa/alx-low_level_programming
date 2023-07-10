52248b7c7ec981ae57c52772844e991a386452f3
Thabisa <tab95mazingisa@gmail.com>
2023-07-10 03:19:10 -0700

    error fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,12 +15,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(filename, O_RONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
-	w - write(STDOUT_FILENO, buf, t);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		count++;
 		results = write(file, text_content, count);
 	}
-		
+
 		if (results == -1)
 			return (-1);
 		close(file);
