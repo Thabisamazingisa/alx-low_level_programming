21f200b8377c96c630811b292f6838f56d696f7e
Thabisa <tab95mazingisa@gmail.com>
2023-08-17 01:23:09 -0700

    Linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,26 @@
 #include "lists.h"
-
 /**
- * print_dlistint - prints all the elements of dlistin_t
- * @h: list head
+ * dlistint_len -returns the number of elements in a linked list
+ * @h: head of the list
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
 
 	count = 0;
+
 	if (h == NULL)
 		return (count);
+
 	while (h->prev != NULL)
 		h = h->prev;
+
 	while (h != NULL)
 	{
-		print("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
